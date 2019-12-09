6d70a2d11bd59bc2b198d880fbea962f022c824e
Luke Lapresi <llapresi@me.com>
2019-12-09 18:32:02 -0500

    installed shortid pkg, implemented extremely basic api stuff

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const shortid = require('shortid');
+const app = express();
+const port = 3000;
+
+// use json
+app.use(express.json());
+
+// Create in memory store for polls
+let polls = [];
 
 app.get('/', (req, res) => res.send('Hello Pointman!'))
+
+app.post('/polls', (req, res) => {
+    let newPoll = req.body;
+    newPoll.id = shortid.generate();
+    polls.push(newPoll);
+    res.json(newPoll);
+});
+
+app.get('/polls/:id', (req, res) => {
+    for(let i = 0; i < polls.length; i++) {
+        if(polls[i].id === req.params.id) {
+            return res.json(polls[i]);
+        }
+    };
+    return res.json('no poll with this id');
+});
+
+app.get('/polls', (req, res) => {
+    res.json(polls);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
