3749d5e22d2b9da89655b5842aaebc6c5f1e6a9e
Luke Lapresi <llapresi@me.com>
2019-12-11 22:35:07 -0500

    Heroku deployment stuff

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const { PollController, VoteController } = require('./controllers');
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 // use json
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello Pointman!'));
-
+// use build directory
+app.use(express.static(path.join(__dirname, 'client/build')));
 // Poll API routes
 app.post('/api/polls', PollController.createPoll);
 // Return polls for id
@@ -21,8 +22,8 @@ app.get('/api/polls/', PollController.getPolls);
 // Vote for a PollOption (using PollOption id)
 app.post('/api/vote', VoteController.makeVote);
 
-// app.get('/polls', (req, res) => {
-//     res.json(polls);
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
