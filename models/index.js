8f254c4aef386d9c4f2ad04062a9b099e41b4b19
Luke Lapresi <llapresi@me.com>
2019-12-11 22:56:17 -0500

    using single DATABASE_URL in dotenv instead of seperate user/pw stuff.

--- models/index.js
+++ models/index.js
@@ -1,10 +1,7 @@
 const Sequelize = require('sequelize');
 
 // Setup our db
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 sequelize
   .authenticate()
   .then(() => {
