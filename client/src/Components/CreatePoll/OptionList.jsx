586de93d13fee9e8f3cb3fd08e15b50e7f1d9a5c
Luke Lapresi <llapresi@me.com>
2020-05-05 21:01:11 -0400

    Poll options now have a minimum of two options.

--- client/src/Components/CreatePoll/OptionList.jsx
+++ client/src/Components/CreatePoll/OptionList.jsx
@@ -17,7 +17,11 @@ const OptionList = ({
         index={index}
         errors={errors}
         register={register}
-        onRemove={() => remove(index)}
+        onRemove={() => {
+          if (options.length >= 3) {
+            remove(index);
+          }
+        }}
       />
     ))}
   </VerticalList>
